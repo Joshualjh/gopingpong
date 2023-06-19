f46ba875d5d3073ec4b55fbd4a8802661d08d1df
Joshualjh <aak1009@naver.com>
2023-06-19 14:05:32 +0900

    set

--- ping/main.go
+++ ping/main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
-			"message": "test2222 ",
+			"message": "rolling update ",
 		})
 	})
 	r.Run(":9000")
